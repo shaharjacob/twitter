02d7007ffdf1f8601c50d3cf07525e52cb72607f
shaharjacob-mac <shahar.j12@gmail.com>
2022-04-02 18:17:03 +0300

    .

--- twitter.py
+++ twitter.py
@@ -1,5 +1,6 @@
 import os
 import time
+import json
 from pathlib import Path
 from typing import Union
 from datetime import datetime, timedelta
@@ -71,37 +72,32 @@ class Twitter(object):
         max_size: int = 0,
         output: Union[str, Path] = 'output.json'
     ) -> dict:
-        # end time should be at least 10 second from the request time (in UTC time)
-        end_time = datetime.now() - timedelta(hours=UTC_DIFF_TIME, seconds=TWITTER_DELAY)
-        # if using non-research API, timedelta should be < 7 days
-        start_time = end_time - time_window
-        
-        payload = {
-            "query": query,
-            "start_time": start_time.strftime("%Y-%m-%dT%H:%M:%SZ"),
-            "end_time": end_time.strftime("%Y-%m-%dT%H:%M:%SZ"),
-            "max_results": MAX_RESULTS_PER_REQUEST,
-        }
-        payload_as_array = [f"{k}={v}" for k,v in payload.items()]
-        payload_as_string = "&".join(payload_as_array)
-        
-        tweets = []
-        number_of_requests = 1
-        # https://developer.twitter.com/en/docs/twitter-api/tweets/search/api-reference/get-tweets-search-recent
-        response = requests.get(
-            f'https://api.twitter.com/2/tweets/search/recent?{payload_as_string}',
-            headers={
-                "Accept": "application/json",
-                "Authorization": f"Bearer {self.token}"
+
+        def dump_tweets(tweets):
+            secho(f"Saving results in {output}", fg="blue")
+            with open(output, 'w') as f:
+                json.dump(tweets, f, indent='\t')
+
+        try:
+            # end time should be at least 10 second from the request time (in UTC time)
+            end_time = datetime.now() - timedelta(hours=UTC_DIFF_TIME, seconds=TWITTER_DELAY)
+            # if using non-research API, timedelta should be < 7 days
+            start_time = end_time - time_window
+            
+            payload = {
+                "query": query,
+                "start_time": start_time.strftime("%Y-%m-%dT%H:%M:%SZ"),
+                "end_time": end_time.strftime("%Y-%m-%dT%H:%M:%SZ"),
+                "max_results": MAX_RESULTS_PER_REQUEST,
             }
-        ).json()
-        tweets.extend(response.get("data", []))
-        next_token = response.get("meta", {"next_token": ""}).get("next_token")
-        while next_token:
-            time.sleep(1) # https://developer.twitter.com/en/docs/twitter-api/rate-limits
-            secho(f"{len(tweets)},", fg="blue", nl=False)
+            payload_as_array = [f"{k}={v}" for k,v in payload.items()]
+            payload_as_string = "&".join(payload_as_array)
+            
+            tweets = []
+            number_of_requests = 1
+            # https://developer.twitter.com/en/docs/twitter-api/tweets/search/api-reference/get-tweets-search-recent
             response = requests.get(
-                f'https://api.twitter.com/2/tweets/search/recent?{payload_as_string}&next_token={next_token}',
+                f'https://api.twitter.com/2/tweets/search/recent?{payload_as_string}',
                 headers={
                     "Accept": "application/json",
                     "Authorization": f"Bearer {self.token}"
@@ -109,10 +105,28 @@ class Twitter(object):
             ).json()
             tweets.extend(response.get("data", []))
             next_token = response.get("meta", {"next_token": ""}).get("next_token")
-            number_of_requests += 1
-            if max_size > 0 and len(tweets) > max_size:
-                tweets = tweets[:max_size]
-                break
+            while next_token:
+                time.sleep(1) # https://developer.twitter.com/en/docs/twitter-api/rate-limits
+                secho(f"{len(tweets)},", fg="blue", nl=False)
+                response = requests.get(
+                    f'https://api.twitter.com/2/tweets/search/recent?{payload_as_string}&next_token={next_token}',
+                    headers={
+                        "Accept": "application/json",
+                        "Authorization": f"Bearer {self.token}"
+                    }
+                ).json()
+                tweets.extend(response.get("data", []))
+                next_token = response.get("meta", {"next_token": ""}).get("next_token")
+                number_of_requests += 1
+                if max_size > 0 and len(tweets) > max_size:
+                    tweets = tweets[:max_size]
+                    break
+        except Exception as e:
+            dump_tweets(tweets)
+            secho(f"[ERROR] {e}", fg="red", bold=True)
+            exit(1)
+        
+        dump_tweets(tweets)
 
         return {
             "number_of_requests": number_of_requests,
